9ae7f62573242ac1a6aec00d42ffa21a7253d1f8
orkaner <plastikt@hotmail.com>
2014-03-25 11:08:17 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveManagerDashboard::Application.config.secret_token = '5b3c04bacb3408e6627b93292a9aa5cb87e3c9fd672dff2f0349632980e097c5bbefa804481e2526951466f80cee93bc4ab7147bf95a96ff0adc2a8e150d2c4a'
